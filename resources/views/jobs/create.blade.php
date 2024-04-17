afe5964e5e7276f4a781496edee3fd61b52a22ab
prestongramberg <knighttraintint@gmail.com>
2024-04-16 19:20:52 -0500

    Add: Components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,20 +12,13 @@
                 <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                    <x-form-feild>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title"
-                                       class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                       placeholder="Shift Leader" required>
-                            </div>
-                            @error('title')
-                            <p class="text-xs text-red-500 font-semibold">{{ $message }}</p>
-                            @enderror
+                           <x-form-input name="title" id="title" placeholder="CEO" required/>
+                            <x-form-error name="title"/>
                         </div>
-                    </div>
+                    </x-form-feild>
 
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
@@ -42,15 +35,15 @@
                         </div>
                     </div>
                 </div>
-{{--                <div class="mt-10">--}}
-{{--                    @if($errors->any())--}}
-{{--                        <ul>--}}
-{{--                            @foreach($errors->all() as $error)--}}
-{{--                                <li class="text-red-500 italic">{{ $error }}</li>--}}
-{{--                            @endforeach--}}
-{{--                        </ul>--}}
-{{--                    @endif--}}
-{{--                </div>--}}
+                <div class="mt-10">
+                    @if($errors->any())
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li class="text-red-500 italic">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div>
             </div>
         </div>
 
