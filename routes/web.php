9b4c3a1f155171d592e623d959381befe52758d2
prestongramberg <knighttraintint@gmail.com>
2024-04-23 00:02:56 -0500

    Fix: Home Page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\SessionController;
 use App\Mail\JobPosted;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
+Route::view('/home', 'home');
 Route::view('/contact', 'contact');
 
 Route::get('/jobs', [JobController::class, 'index']);
